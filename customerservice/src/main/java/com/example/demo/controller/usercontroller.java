6030fb71d431f4759d33b1f62e7939baa3c5305d
saibhargav3322 <81942988+saibhargav3322@users.noreply.github.com>
2022-04-21 10:06:40 +0530

    Add files via upload

---
+++ customerservice/src/main/java/com/example/demo/controller/usercontroller.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class usercontroller {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+}
