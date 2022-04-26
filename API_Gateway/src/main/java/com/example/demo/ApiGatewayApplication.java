ed51ba6f2f717014adc276fb8eba95f0a72764fc
VamsiMallipudi <85820488+VamsiMallipudi@users.noreply.github.com>
2022-04-26 12:22:14 +0530

    Add files via upload

---
+++ API_Gateway/src/main/java/com/example/demo/ApiGatewayApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@EnableEurekaClient
+@EnableSwagger2
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ API_Gateway/src/main/java/com/example/demo/swagger/SwaggerProvider.java
@@ -0,0 +1,52 @@
+package com.example.demo.swagger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+@Component
+@Primary
+
+public class SwaggerProvider implements SwaggerResourcesProvider {
+	
+	//private static Logger log = LoggerFactory.getLogger(SwaggerProvider.class);
+    
+    public static final String API_URI = "/v2/api-docs";
+    
+    private final RouteDefinitionLocator routeLocator;
+    
+    public SwaggerProvider(RouteDefinitionLocator routeLocator) {
+        this.routeLocator = routeLocator;
+    }
+    
+    @Override
+    
+    public List<SwaggerResource> get() {
+        List<SwaggerResource> resources = new ArrayList<>();
+        routeLocator.getRouteDefinitions().subscribe(routeDefinition -> {
+            //log.info("Discovered route definition: {}", routeDefinition.getId());
+            String resourceName = routeDefinition.getId();
+            String location = routeDefinition.getPredicates().get(0).getArgs().get("_genkey_0").replace("/**", API_URI);
+           // log.info("Adding swagger resource: {} with location {}", resourceName, location);
+            resources.add(swaggerResource(resourceName, location));
+        });
+        
+        return resources;
+    }
+    
+    private SwaggerResource swaggerResource(String name, String location) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion("2.0");
+        return swaggerResource;    
+    }
+}
