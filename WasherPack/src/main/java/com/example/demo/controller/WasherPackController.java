4c9009fe42b979af5fb14266f09e7940d6e38cf3
VamsiMallipudi <85820488+VamsiMallipudi@users.noreply.github.com>
2022-04-26 12:25:21 +0530

    Added WasherPack

---
+++ WasherPack/src/main/java/com/example/demo/controller/WasherPackController.java
@@ -0,0 +1,50 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.WasherPack;
+import com.example.demo.repository.WasherRepository;
+import com.example.demo.service.WasherPackService;
+
+
+
+
+@RestController
+@RequestMapping("/washerPack")
+public class WasherPackController {
+	
+	@Autowired
+	private WasherRepository washerRepository;
+	
+	@Autowired
+	private WasherPackService washerPackService;
+	
+	//add pack
+		@PostMapping("/addpack")
+		public String savePack(@RequestBody WasherPack pack) {
+			washerRepository.save(pack);
+			return "Pack saved successfully with id: "+pack.getId();
+		}
+		
+		//get all packs
+		@GetMapping("/allpacks")
+		public List<WasherPack> getPack(){
+			return washerRepository.findAll();
+		}
+		
+		//delete pack by id
+		@DeleteMapping("/deleteWash/{id}")
+		public String deletePack(@PathVariable int id) {
+			washerRepository.deleteById(id);
+			return "Pack deleted with id: "+id;
+		}
+}
