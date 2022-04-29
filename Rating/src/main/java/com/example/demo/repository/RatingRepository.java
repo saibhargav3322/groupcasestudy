bef2b9414f417188e69091fc90ce755c8976c222
saibhargav3322 <81942988+saibhargav3322@users.noreply.github.com>
2022-04-29 11:48:10 +0530

    pre final

---
+++ Rating/src/main/java/com/example/demo/repository/RatingRepository.java
@@ -0,0 +1,21 @@
+package com.example.demo.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.Aggregation;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.example.demo.model.Ratings;
+
+
+
+public interface RatingRepository extends MongoRepository<Ratings, Integer>{
+
+	//Optional<Ratings> findBywasherid(int washerid);
+
+	
+
+	
+}
---
+++ Rating/src/main/java/com/example/demo/service/RatingService.java
@@ -0,0 +1,56 @@
+package com.example.demo.service;
+
+import java.util.ArrayList;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Queue;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.repository.Aggregation;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Ratings;
+import com.example.demo.repository.RatingRepository;
+
+
+@Service
+public class RatingService {
+	
+	@Autowired
+	private RatingRepository ratingRepository;
+	
+	//add rating
+	public Ratings addRating(Ratings rating) {
+		return ratingRepository.save(rating);
+	}
+	
+	//view all ratings
+	public List<Ratings> allRatings(){
+		List<Ratings> ratings = ratingRepository.findAll();
+		System.out.println("Getting Ratings from data base:"+ratings);
+		return ratings;
+	}
+	
+	
+	//view all ratings by id
+	public List<Ratings> allRatingsbyusername(String washerusername){
+		List<Ratings> ratings = ratingRepository.findAll();
+		List<Ratings> a=new ArrayList<>();
+		for(int i=0;i<ratings.size();i++)
+		{
+			Ratings w=new Ratings();
+			w=ratings.get(i);
+			if(w.getWasher_username().equals(washerusername))
+			{
+				//System.out.println(w.getLocation());
+				a.add(ratings.get(i));
+			}
+		}
+		return a;
+	}
+}
