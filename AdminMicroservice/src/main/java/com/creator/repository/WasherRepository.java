09a93de7eef961683d27964df695f78252117069
saibhargav3322 <81942988+saibhargav3322@users.noreply.github.com>
2022-04-29 11:42:29 +0530

    Delete AdminMicroservice directory

--- AdminMicroservice/src/main/java/com/creator/repository/WasherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.creator.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.creator.model.WasherPack;
-
-public interface WasherRepository extends MongoRepository<WasherPack, Integer>{
-
-}
