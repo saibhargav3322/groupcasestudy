acc8dd68ece24945cb55d3e76f3af9b91f307bea
saibhargav3322 <81942988+saibhargav3322@users.noreply.github.com>
2022-04-29 11:51:28 +0530

    Delete OrderMicroservice directory

--- OrderMicroservice/src/main/java/com/example/demo/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.demo.model.OrderDetails;
-
-public interface OrderRepository extends MongoRepository<OrderDetails, Integer>{
-
-}
